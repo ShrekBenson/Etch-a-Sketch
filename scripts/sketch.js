16404f81746b758a485cf39ba8d95be8d829b121
Diego Antonio Angel Rodriguez <daardibujos@gmail.com>
2023-10-02 14:21:45 -0600

    Add rezise feature to the grid

--- scripts/sketch.js
+++ scripts/sketch.js
@@ -6,9 +6,43 @@ SQUARE.classList.add('square');
 
 function makeGrid(size){
   let grid = size**2
+  let width = 0;
+  let height = 0;
+
+  switch (grid) {
+    case 32**2:      
+      width = 1;
+      height = 1;      
+      break;
+    
+    case 48**2:
+      width = .6666;
+      height = .6666;      
+      break;
+    
+    case 64**2:      
+      width = .5;
+      height = .5;      
+      break;
+    
+    case 80**2:      
+      width = .4;
+      height = .4;      
+      break;
+    
+    case 96**2:      
+      width = .3334;
+      height = .3334;      
+      break;
+    
+    default:
+      width = 2;
+      height = 2;      
+  }
+
   for (let i = 0; i < grid; i++) {
     const SQUARE = document.createElement('div');
-    SQUARE.classList.add('square');
+    SQUARE.style.cssText = `width: ${width}rem; height: ${height}rem; border: 1px solid black`;
     CANVAS.appendChild(SQUARE);
   }
 }
@@ -30,23 +64,12 @@ CANVAS.addEventListener('mousemove', (e) =>{
     e.target.classList.add('draw');    
   }
 })
+
 window.addEventListener('DOMContentLoaded', () => {
-  makeGrid(SIZE.value);
-  
+  makeGrid(SIZE.value);  
 })
 window.addEventListener('input', () => {
   CANVAS.innerHTML = '';
   makeGrid(SIZE.value);
-  OUTPUT.textContent = `${SIZE.value}px`;
-  switch (SIZE.value) {
-    case 32:      
-      SQUARE.style.cssText = 'width: 1rem;';
-      SQUARE.style.cssText = 'height: 1rem;';
-      break;
-  
-    default:
-      SQUARE.style.cssText = 'width: 2rem;';
-      SQUARE.style.cssText = 'height: 2rem;';
-      break;
-  }
+  OUTPUT.textContent = `${SIZE.value}px`;  
 })
